0a9794feb57522220b566c36c9c53dd541479933
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-07-08 11:32:17 +0200

    Add MUI, Emotion, and React Router setup
    Added @emotion/react, @emotion/styled, react-router, and react-router-dom dependencies. Updated App.jsx to include CssBaseline from MUI and prepared for routing. Modified main.jsx to wrap App in a div. Removed public/index.html, likely to migrate to a different static file setup.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div>App</div>
+
+    <div>
+      <CssBaseline />
+      <h1>Filmpire</h1>
+    </div>
   )
 }
 
-export default App
+export default App;
